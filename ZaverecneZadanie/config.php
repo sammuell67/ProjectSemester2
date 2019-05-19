0c575e3c538e94fc7ced305ec3f32304b995942a
matus <chalko.mat@gmail.com>
2019-05-19 16:28:14 +0200

    Upravy login

--- ZaverecneZadanie/config.php
+++ ZaverecneZadanie/config.php
@@ -9,4 +9,10 @@ $servername = "localhost";
 $username = "meno_noveho_administratora";
 $password = "Smatlab";
 $dbname = "ZaverecneZadanie";
+
+$conn = new PDO("mysql:host=$servername;dbname=ZaverecneZadanie;charset=utf8", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+
+
 ?>
